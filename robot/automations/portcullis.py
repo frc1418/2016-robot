e9d49fd9bc9f80090e99db4d0a7e562880783e93
Timothy S. Winters <twinters007@gmail.com>
2016-01-18 23:21:15 -0500

    Added timer for driving

--- robot/automations/portcullis.py
+++ robot/automations/portcullis.py
@@ -1,5 +1,7 @@
+import wpilib
 ARM_DOWN = 1
-ARM_UP = 2
+DRIVE = 2
+ARM_UP = 3
 class PortcullisLift:
  
     def __init__(self, drive, intake, drive_speed=-.5):
@@ -11,7 +13,7 @@ class PortcullisLift:
         self.is_running = False
         self.state = ARM_DOWN
         
-        
+        self.timer = wpilib.Timer()
     def get_running(self):
         return self.is_running
     
@@ -22,9 +24,13 @@ class PortcullisLift:
         if self.state == ARM_DOWN:
             self.intake.set_arm_bottom()
             if self.intake.on_target():
+                self.timer.reset()
+                self.state = DRIVE
+        if self.state == DRIVE:
+            self.drive.move(self.drive_speed, 0)
+            if self.timer.hasPeriodPassed(1):
                 self.state = ARM_UP
         if self.state == ARM_UP:
-            self.drive.move(self.drive_speed, 0)
             self.intake.set_arm_top()
             if self.intake.on_target():
                 self.is_running = False
