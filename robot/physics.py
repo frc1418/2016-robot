0e28c410eabc92adbfb6d19079e9611e77aeb91e
Timothy S. Winters <twinters007@gmail.com>
2016-01-19 22:11:16 -0500

    Second arm works like a dream now. Still don't run with auto-mode

--- robot/physics.py
+++ robot/physics.py
@@ -19,10 +19,8 @@ class PhysicsEngine:
         try:
             
             armDict = hal_data['CAN'][25] # armDict is the dictionary of variables assigned to CANTalon 25
-            followArmDict = hal_data['CAN'][30]
             armPercentVal = int(armDict['value']*tm_diff*300/1023) # Encoder position increaser when in PercentVbus mode based on time and thrust value
             posVal = int(tm_diff*300) # Encoder Position difference when in Position mode based on time
-            armDict['limit_switch_closed_for'] = False 
         except:
             pass
         
@@ -42,8 +40,8 @@ class PhysicsEngine:
               
             if self.armAct in range(1390, 1400):
                 armDict['limit_switch_closed_rev'] = True
-            followArmDict['enc_position'] = armDict['enc_position']
-            armDict['enc_position'] = max(min(armDict['enc_position'], 1440), 0) # Keep encoder between these values
+                
+            armDict['enc_position'] = max(min(self.armAct, 1440), 0) # Keep encoder between these values
             
         # Simulate the drivetrain
         lf_motor = hal_data['CAN'][5]['value']
@@ -57,4 +55,4 @@ class PhysicsEngine:
         rr_motor = max(min(rr_motor, 1), -1)
         
         fwd, rcw = four_motor_drivetrain(lr_motor, rr_motor, lf_motor, rf_motor)
-        #self.controller.drive(fwd, rcw, tm_diff)
+        self.controller.drive(fwd, rcw, tm_diff)
