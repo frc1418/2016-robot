25ee3ed284f99ad9f95b83c7e0654134eb68ad1b
Timothy S. Winters <twinters007@gmail.com>
2016-02-06 23:35:12 -0500

    Added PID physics

--- robot/components/intake.py
+++ robot/components/intake.py
@@ -62,7 +62,7 @@ class Arm:
           ]
         self.position_threshold = self.sd.getAutoUpdateValue("Arm|On Target Threshold", 170)
         self.wanted_pid = (
-            self.sd.getAutoUpdateValue('Arm |P', 2),
+            self.sd.getAutoUpdateValue('Arm |P', 15),
             self.sd.getAutoUpdateValue('Arm |I', 0), 
             self.sd.getAutoUpdateValue('Arm |D', 0)
         )
