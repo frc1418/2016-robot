fa91958ee844aa560a87aabc8f660c2ab8b26a7f
Killerhamster222 <matt.puentes@gmail.com>
2016-04-25 23:41:19 -0400

    Added Ball Modular Autonomus, which takes a ball, turns around, and runs the rest of modular normally.

--- robot/autonomous/ModularAutonomous.py
+++ robot/autonomous/ModularAutonomous.py
@@ -102,3 +102,69 @@ class ModularAutonomous(LowBar, ChevalDeFrise, Portcullis, Charge, Default):
         self.drive.drive_distance(-2.5)
         self.intake.outtake()
                
+class BallModularAutonomous(ModularAutonomous):   
+    MODE_NAME = "Ball_Modular_Autonomous"
+    DEFAULT = False
+    
+    sd = NetworkTable
+    intake = Intake.Arm
+    drive = Drive.Drive
+
+    def initialize(self):
+        LowBar.initialize(self)
+        ChevalDeFrise.initialize(self)
+        Portcullis.initialize(self)
+        
+        self.register_sd_var('Drive_Distance', 10.5)
+        self.register_sd_var('Rotate_Angle', 180)
+        self.register_sd_var('Collect_Distance', 0.5)
+
+    @state(first=True)
+    def startModularAutonomous(self):
+        print(self.sd.getValue('robotDefense', 'Default') + 'Start')
+        self.intake.manualZero()
+        self.drive.reset_gyro_angle()
+        self.next_state('drive_to_ball')
+        
+    @timed_state(duration = 2, next_state='lower_arms')
+    def drive_to_ball(self, initial_call):
+        if initial_call:
+            self.drive.reset_drive_encoders()
+            
+        if self.drive.drive_distance(self.Drive_Distance):
+            self.next_state('lower_arms')
+            
+    @timed_state(duration = 1, next_state='collect')
+    def lower_arms(self, initial_call):
+        self.intake.set_arm_middle()
+        if self.intake.on_target(): 
+            self.next_state('collect')
+    
+    @state
+    def collect(self, initial_call):
+        if initial_call:
+            self.drive.reset_drive_encoders()
+            
+        self.intake.intake()
+        if self.drive.drive_distance(self.Collect_Distance):
+            self.next_state('back_up')
+
+    @state
+    def back_up(self,initial_call):
+        if initial_call:
+            self.drive.reset_drive_encoders()
+            
+        if self.drive.drive_distance(-(self.Collect_Distance+self.Drive_Distance)):
+            self.next_state('turn_around')
+        
+    @state
+    def turn_around(self,initial_call):
+        if initial_call:
+            self.drive.reset_gyro_angle()
+            
+        if self.drive.angle_rotation(self.Rotate_Angle):
+            self.next_state(self.sd.getValue('robotDefense', 'LowBar') + 'Start')
+            self.position = int(self.sd.getValue('robotPosition', '1'))
+            print('test '+(self.sd.getValue('robotDefense', 'LowBar') + 'Start'))
+            print('test '+self.sd.getValue('robotPosition', '1'))
+    
